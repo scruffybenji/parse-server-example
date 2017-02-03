4ffd3ae51cf5d3bd25be37581c30fd2d3f2c8f78
Justin Woo (Jibo) <justinwoo-jibo@Justins-MBP-2.8.8.8.8>
2017-02-02 18:53:37 -0800

    commented out require of other files

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,6 @@
-require('cloud/installation.js');
-require('cloud/activity.js');
-require('cloud/photo.js');
+//require('cloud/installation.js');
+//require('cloud/activity.js');
+//require('cloud/photo.js');
 
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
